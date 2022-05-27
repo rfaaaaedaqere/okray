9e82a81129bc051d6d1e46324c716e371525cf0e
rfaaaaedaqere <93361527+rfaaaaedaqere@users.noreply.github.com>
2022-05-27 12:14:33 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-ID=ad806487-2d26-4636-98b6-ab85cc8521f7
+ID=ff9a29f6-279e-4c1c-8b51-c58775224168
 AID=64
-WSPATH=/
+WSPATH=/okray
 PORT=80
 
 # Write V2Ray configuration
